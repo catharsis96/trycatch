e3f969dd7852eaaa673ee9d12040b882ba74a974
catharsis96 <catharsis96@yandex.ru>
2017-06-12 21:11:55 +0300

    Add project files.

---
+++ GracefullyHandlingExceptions/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GracefullyHandlingExceptions
+{
+    class trycatch
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string content = File.ReadAllText(@"D:\Lesson22\Exampl.txt");
+                Console.WriteLine(content);
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine("There is a problem!");
+                Console.WriteLine("Make sure the name of the file is named correctly: Example.txt");
+            }
+            catch(DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("There is a problem!");
+                Console.WriteLine(@"Make sure the directory D:\Lesson22 exists");
+            }
+            //the most important catch, show you an issue
+            catch (Exception ex)
+            {
+                Console.WriteLine("There is a problem!");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                //Code to finalize
+                //Setting objects to null
+                //Closing database connections
+                Console.WriteLine("Closing application now ... ");
+            }
+        }
+    }
+}
---
+++ GracefullyHandlingExceptions/trycatch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GracefullyHandlingExceptions
+{
+    class trycatch
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string content = File.ReadAllText(@"D:\Lesson22\Exampl.txt");
+                Console.WriteLine(content);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("There is a problem!");
+                Console.WriteLine(ex.Message);
+            }
+           
+        }
+    }
+}
